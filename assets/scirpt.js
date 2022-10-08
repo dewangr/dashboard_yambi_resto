07a952fb52f200f36bbf403bb03d2cd5e1a8ef70
I Dewa Ngurah Tri Hendrawan <65481663+dewangr@users.noreply.github.com>
2022-10-09 02:02:13 +0800

    need improve

---
+++ assets/scirpt.js
@@ -0,0 +1,18 @@
+// window scroll 
+const nav = document.querySelector('nav');
+window.addEventListener('scroll', function(){
+    this.scrollY > 20 ? nav.classList.add('active') : nav.classList.remove('active');
+});
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener(('click'), () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener(('click'), () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}));
